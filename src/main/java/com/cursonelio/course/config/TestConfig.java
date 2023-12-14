fdf39acfa3763b3a41bad468d97fa22ff7c869ee
Thiago Santos <tsantos7098@gmail.com>
2023-12-14 01:06:25 -0300

    Product

--- src/main/java/com/cursonelio/course/config/TestConfig.java
+++ src/main/java/com/cursonelio/course/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.cursonelio.course.config;
 
 import com.cursonelio.course.entities.Category;
 import com.cursonelio.course.entities.Order;
+import com.cursonelio.course.entities.Product;
 import com.cursonelio.course.entities.User;
 import com.cursonelio.course.entities.enums.OrderStatus;
 import com.cursonelio.course.repositories.CategoryRepository;
 import com.cursonelio.course.repositories.OrderRepository;
+import com.cursonelio.course.repositories.ProductRepository;
 import com.cursonelio.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -25,23 +27,40 @@ public class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Thiago Santos", "thiago@gmail.com", "82987063937", "123456");
         User u2 = new User(null, "Jose Silva", "jose@gmail.com", "82987456241", "123458");
+        userRepository.saveAll(Arrays.asList(u1,u2));
 
         Order o1 = new Order(null, Instant.parse("2023-12-11T18:37:10Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2023-10-25T10:24:45Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2023-12-11T07:50:36Z"), OrderStatus.WAITING_PAYMENT,u1);
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
-
-        userRepository.saveAll(Arrays.asList(u1,u2));
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
     }
 
 }
